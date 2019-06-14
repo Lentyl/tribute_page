02477aaec02d8f91aff0e5deed86ca8342af7123
Mariusz Podgorski <m.b.podgorski1984@gmail.com>
2019-06-14 21:36:08 +0100

    2 switch animation

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 const screen = document.querySelector('.tvOff');
 const tvSwitch = document.querySelector('.switch');
-
-console.log(tvSwitch);
+const fullscreenBT = document.querySelector('.switch2');
+const youTub = document.querySelector('.youTub')
 
 tvSwitch.addEventListener('click', () => {
     screen.style.zIndex = '0';
     tvSwitch.style.transform = 'rotate(45deg)';
 });
+
+//fullscreen
+
+fullscreenBT.addEventListener('click', () => {
+    youTub.webkitRequestFullscreen(); //wywołuje fullscreen
+})
